2d06d373899e5a87c07836c3fb4b719de079bf5b
lovexyn0827 <2717498529@qq.com>
2022-08-10 15:06:34 +0800

    1.16.4-20220810-BETA

--- src/main/java/lovexyn0827/mess/log/EntityLogger.java
+++ src/main/java/lovexyn0827/mess/log/EntityLogger.java
@@ -36,6 +36,7 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.server.MinecraftServer;
 
 // TODO Support for EnderDragonPart, whose EntityType is not specified
+// TODO Support for specifying a TickingPhase
 public final class EntityLogger {
 	Int2ObjectMap<EntityHolder> entities = new Int2ObjectOpenHashMap<>();
 	private Map<String, ListenedField> customFields = new HashMap<>();
