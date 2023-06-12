dac69b82816eaa0096a9ecb7b493c5e1c62a6eb9
lovexyn0827 <2717498529@qq.com>
2023-06-12 23:43:56 +0800

    fix: /freezentity prevents the chunk position of entities from updating

---
+++ src/main/java/lovexyn0827/mess/mixins/ServerChunkManagerMixin.java
@@ -0,0 +1,21 @@
+package lovexyn0827.mess.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import lovexyn0827.mess.command.FreezeEntityCommand;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.world.ServerChunkManager;
+
+@Mixin(ServerChunkManager.class)
+public class ServerChunkManagerMixin {
+	@Inject(method = "shouldTickEntity", at = @At("HEAD"), cancellable = true)
+	private void tickEntityIfNeeded(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+		if(FreezeEntityCommand.FROZEN_ENTITIES.contains(entity)) {
+			cir.setReturnValue(false);
+			cir.cancel();
+		}
+	}
+}
