eceb1af39e620f9f81283b5e0ed44846eb02a3b8
lovexyn0827 <2717498529@qq.com>
2023-08-01 11:25:32 +0800

    fix: When using key bindings F3 + <Key>, actions attached to <Key> are performed. For example, when pressing F3 + E, the inventory screen is opened.

--- src/main/java/lovexyn0827/mess/mixins/KeyboardMixin.java
+++ src/main/java/lovexyn0827/mess/mixins/KeyboardMixin.java
@@ -25,22 +25,42 @@ import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
 public abstract class KeyboardMixin {
 	@Shadow @Final MinecraftClient client;
 	
-	@Inject(method = "processF3", at = @At(value = "HEAD"))
-	private void onF3Pressed(int key, CallbackInfoReturnable<?> ci) {
-		if(key == 'E') {
+	@Inject(method = "processF3", at = @At(value = "HEAD"), cancellable = true)
+	private void onF3Pressed(int key, CallbackInfoReturnable<Boolean> cir) {
+		switch(key) {
+		case 'E':
 			LookingAtEntityHud lookingHud = MessMod.INSTANCE.getClientHudManager().lookingHud;
-			if(lookingHud != null) lookingHud.toggleRender();
-			this.client.player.sendChatMessage(I18N.translate("hud.target") + (lookingHud.shouldRender ? "On" : "Off"));
-		} else if(key == 'M') {
-			PlayerHud playerHud = MessMod.INSTANCE.getClientHudManager().playerHudC;
-			if(playerHud != null) playerHud.toggleRender();
-			this.client.player.sendChatMessage(I18N.translate("hud.client") + (playerHud.shouldRender ? "On" : "Off"));
-		} else if(key == 'S') {
-			PlayerHud playerHud = MessMod.INSTANCE.getClientHudManager().playerHudS;
-			if(playerHud == null) return;
-			playerHud.toggleRender();
-			this.client.player.sendChatMessage(I18N.translate("hud.server") + (playerHud.shouldRender ? "On" : "Off"));
+			if(lookingHud != null) {
+				lookingHud.toggleRender();
+				this.client.player.sendChatMessage(
+						I18N.translate("hud.target") + (lookingHud.shouldRender ? "On" : "Off"));
+			}
+			
+			break;
+		case 'M':
+			PlayerHud playerHudC = MessMod.INSTANCE.getClientHudManager().playerHudC;
+			if(playerHudC != null) {
+				playerHudC.toggleRender();
+				this.client.player.sendChatMessage(
+						I18N.translate("hud.client") + (playerHudC.shouldRender ? "On" : "Off"));
+			}
+			
+			break;
+		case 'S':
+			PlayerHud playerHudS = MessMod.INSTANCE.getClientHudManager().playerHudS;
+			if(playerHudS != null) {
+				playerHudS.toggleRender();
+				this.client.player.sendChatMessage(
+						I18N.translate("hud.server") + (playerHudS.shouldRender ? "On" : "Off"));
+			}
+			
+			break;
+		default :
+			return;
 		}
+		
+		cir.setReturnValue(true);
+		cir.cancel();
 	}
 	
 	@Inject(method = "onKey", at = @At("RETURN"))
