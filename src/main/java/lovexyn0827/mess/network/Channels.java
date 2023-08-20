4aedd7294d7b1d1ca8b66df690d8004acdbbc920
lovexyn0827 <2717498529@qq.com>
2023-08-20 18:37:01 +0800

    Update channel version for command argument type serializerer

--- src/main/java/lovexyn0827/mess/network/Channels.java
+++ src/main/java/lovexyn0827/mess/network/Channels.java
@@ -3,7 +3,7 @@ package lovexyn0827.mess.network;
 import net.minecraft.util.Identifier;
 
 public interface Channels {
-	public static int CHANNEL_VERSION = 2;	// TODO Remember to update the channel version if necessary
+	public static int CHANNEL_VERSION = 3;	// TODO Remember to update the channel version if necessary
 	Identifier SHAPE = new Identifier("messmod", "shape");
 	Identifier HUD = new Identifier("messmod", "hud");
 	Identifier VERSION = new Identifier("messmod", "version");
