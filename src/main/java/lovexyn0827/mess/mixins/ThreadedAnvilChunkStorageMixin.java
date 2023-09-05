078a927f30ba811d3b02e19496c218d8c0f1ea67
lovexyn0827 <2717498529@qq.com>
2023-09-05 23:06:37 +0800

    doc: grammar & spelling

--- src/main/java/lovexyn0827/mess/mixins/ThreadedAnvilChunkStorageMixin.java
+++ src/main/java/lovexyn0827/mess/mixins/ThreadedAnvilChunkStorageMixin.java
@@ -76,7 +76,7 @@ public abstract class ThreadedAnvilChunkStorageMixin {
 			return;
 		}
 		
-		MessMod.INSTANCE.getChunkLogger().onEvent(ChunkEvent.SCHEDULER_UPGARDE, holder.getPos().toLong(), 
+		MessMod.INSTANCE.getChunkLogger().onEvent(ChunkEvent.SCHEDULER_UPGRADE, holder.getPos().toLong(), 
 				this.world.getRegistryKey().getValue(), Thread.currentThread(), 
 				StackTrace.blameCurrent(), requiredStatus.getId());
 	}
@@ -131,7 +131,7 @@ public abstract class ThreadedAnvilChunkStorageMixin {
 			return;
 		}
 		
-		MessMod.INSTANCE.getChunkLogger().onEvent(ChunkEvent.UPGARDE, holder.getPos().toLong(), 
+		MessMod.INSTANCE.getChunkLogger().onEvent(ChunkEvent.UPGRADE, holder.getPos().toLong(), 
 				this.world.getRegistryKey().getValue(), Thread.currentThread(), 
 				StackTrace.blameCurrent(), status.getId());
 	}
@@ -186,7 +186,7 @@ public abstract class ThreadedAnvilChunkStorageMixin {
 			return;
 		}
 		
-		MessMod.INSTANCE.getChunkLogger().onEvent(ChunkEvent.END_UPGARDE, holder.getPos().toLong(), 
+		MessMod.INSTANCE.getChunkLogger().onEvent(ChunkEvent.END_UPGRADE, holder.getPos().toLong(), 
 				this.world.getRegistryKey().getValue(), Thread.currentThread(), StackTrace.blameCurrent(), status.getId());
 	}
 }
