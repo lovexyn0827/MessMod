07c6c21c4cf2575b867b63ae67514cdf86c1b0f6
lovexyn0827 <2717498529@qq.com>
2025-08-27 02:44:29 +0800

    feature: recording & stats in /logpacket

--- src/main/java/lovexyn0827/mess/mixins/ClientConnectionMixin.java
+++ src/main/java/lovexyn0827/mess/mixins/ClientConnectionMixin.java
@@ -11,9 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.GenericFutureListener;
-import lovexyn0827.mess.MessMod;
 import lovexyn0827.mess.command.LogPacketCommand;
-import lovexyn0827.mess.util.deobfuscating.Mapping;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.NetworkSide;
 import net.minecraft.network.Packet;
@@ -26,35 +24,14 @@ public class ClientConnectionMixin {
 	@Inject(method = "sendImmediately", at = @At("HEAD"))
 	private void onPacketBeingSended(Packet<?> packet, GenericFutureListener<? extends Future<? super Void>> callback, CallbackInfo ci) {
 		if(LogPacketCommand.isSubscribed(packet)) {
-			MessMod.LOGGER.info("{}: Sended Packet: {}", 
-					this.side == NetworkSide.CLIENTBOUND ? "CLIENT" : "SERVER", 
-					toString(packet));
+			LogPacketCommand.onPacket(this.side, packet, true);
 		}
 	}
 	
 	@Inject(method = "channelRead0", at = @At("HEAD"))
 	private void onPacketBeingSended(ChannelHandlerContext channelHandlerContext, Packet<?> packet, CallbackInfo ci) {
 		if(LogPacketCommand.isSubscribed(packet)) {
-			MessMod.LOGGER.info("{}: Received Packet: {}", 
-					this.side == NetworkSide.CLIENTBOUND ? "CLIENT" : "SERVER", 
-					toString(packet));
+			LogPacketCommand.onPacket(this.side, packet, false);
 		}
-	}
-	
-	private static String toString(Packet<?> packet) {
-		Mapping mapping = MessMod.INSTANCE.getMapping();
-		StringBuilder sb = new StringBuilder(mapping.simpleNamedClass(packet.getClass().getName()));
-		sb.append('[');
-		for(Field f : packet.getClass().getDeclaredFields()) {
-			f.setAccessible(true);
-			try {
-				sb.append(mapping.namedField(f.getName())).append('=').append(f.get(packet)).append(',');
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		
-		sb.append(']');
-		return sb.toString();
 	}
 }
